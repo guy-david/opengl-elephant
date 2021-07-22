b2dda652f9414661ef8f13bc13d5725046a5a030
Guy David <guy.david@nextsilicon.com>
2021-07-23 02:09:11 +0300

    Re-organize to src and include directories
    Signed-off-by: Guy David <guy.david@nextsilicon.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     constexpr int window_width = 1366;
     constexpr int window_height = 768;
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE | GLUT_STENCIL);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH |
+                        GLUT_MULTISAMPLE | GLUT_STENCIL);
 
     glutInitWindowSize(window_width, window_height);
     glutInitWindowPosition(

be1183accf5524b837d83f913554214923ac973c
Guy David <guy.david@nextsilicon.com>
2021-07-23 02:40:12 +0300

    Add window display function
    Signed-off-by: Guy David <guy.david@nextsilicon.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 
 static int g_window_id;
 
+void on_display()
+{
+    glClearColor( 0.0, 0.0, 0.0, 1.0 );
+    glClear( GL_COLOR_BUFFER_BIT );
+
+    glFlush();
+}
+
 int main(int argc, char *argv[])
 {
     constexpr int window_width = 1366;
@@ -19,6 +27,7 @@ int main(int argc, char *argv[])
         (glutGet(GLUT_SCREEN_HEIGHT) - window_height) / 2);
 
     g_window_id = glutCreateWindow("OpenGL Elephant");
+    glutDisplayFunc(on_display);
 
     glutMainLoop();
 

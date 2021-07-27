4fa04d9cff28fabc55023ffda072d3487cac52e6
Guy David <guy.david@nextsilicon.com>
2021-07-27 22:35:47 +0300

    LIGHT
    Signed-off-by: Guy David <guy.david@nextsilicon.com>

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,18 @@ static GLfloat up[3] = { 0.0f, 1.0f, 0.0f };
 
 static GLfloat ambient_intensity = 0.2f;
 
+struct Material
+{
+    float shininess;
+    vec3 ambience;
+    vec3 diffuse;
+    vec3 specular;
+    vec3 emission;
+};
+
 struct Face
 {
-    size_t material_index;
+    Material material;
     std::vector<std::pair<vec3, vec3>> vertices;
 };
 
@@ -49,15 +58,6 @@ struct Object
 };
 
 std::vector<Object> objects;
-
-struct Material
-{
-    float shininess;
-    vec3 ambience;
-    vec3 diffuse;
-    vec3 specular;
-    vec3 emission;
-};
 
 std::unordered_map<std::string, Material> load_mtl(const std::string &filename)
 {
@@ -126,6 +126,8 @@ Object load_obj(vec3 pos, const std::string &filename)
     }
 
     std::unordered_map<std::string, Material> materials;
+    std::string current_mat;
+
     Object obj;
     obj.pos = pos;
 
@@ -140,6 +142,10 @@ Object load_obj(vec3 pos, const std::string &filename)
             std::string mtl_path = line.substr(7);
             std::string dir_name = filename.substr(0, filename.find_last_of("\\/"));
             materials = load_mtl(dir_name + "/" + mtl_path);
+        }
+        else if (line.substr(0, 7) == "usemtl ")
+        {
+            current_mat = line.substr(7);
         }
         else if (line.substr(0, 2) == "v ")
         {
@@ -164,6 +170,8 @@ Object load_obj(vec3 pos, const std::string &filename)
             std::istringstream ss(line.substr(2));
             std::string token;
             Face face;
+
+            face.material = materials[current_mat];
 
             while (std::getline(ss, token, ' '))
             {
@@ -233,17 +241,19 @@ static void draw_object(Object& obj)
 	glPushMatrix();
 	glNormal3d(0, 1, 0);
 
-    GLfloat color1[4] = { 0.318f, 0.212f, 0.102f, 1.0f };
-	GLfloat color2[4] = { 1.0f, 1.0f, 0.6f, 1.0f };
-
     glTranslatef(obj.pos.x, obj.pos.y, obj.pos.z);
     // glScalef(4.0f, 1.0f, 4.0f);
     for (size_t i = 0; i < obj.faces.size(); i++)
     {
         glBegin(GL_POLYGON);
-	    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (i < 6) ? color1 : color2);
 
-        const auto& face = obj.faces[i];
+        const Face& face = obj.faces[i];
+
+	    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (const float *)&face.material.diffuse);
+        glMaterialfv(GL_FRONT, GL_SPECULAR, (const float *)&face.material.specular);
+        glMaterialfv(GL_FRONT, GL_EMISSION, (const float *)&face.material.emission);
+        glMaterialf (GL_FRONT, GL_SHININESS, face.material.shininess);
+
         for (size_t j = 0; j < face.vertices.size(); j++)
         {
             const vec3 vertex = face.vertices[j].first;
@@ -283,6 +293,23 @@ static void on_display()
 	glShadeModel(GL_SMOOTH);
 	glEnable(GL_NORMALIZE);
 	glEnable(GL_LIGHTING);
+
+    {
+        glEnable(GL_LIGHT0);
+        int id = GL_LIGHT0;
+        float color[3] = { 1.0f, 1.0f, 1.0f };
+        float position[3] = { 0.0f, 15.0f, 0.0f };
+        float target[3] = { 0.0f, 0.0f, 0.0f };
+        glLightfv(id, GL_DIFFUSE, color);
+        glLightfv(id, GL_SPECULAR, color);
+        glLightfv(id, GL_POSITION, position);
+        GLfloat direction[3] = { target[0] - position[0],
+                                target[1] - position[1],
+                                target[2] - position[2] };
+        glLightfv(id, GL_SPOT_DIRECTION, direction);
+        glLightf(id, GL_SPOT_CUTOFF, 120.0f);
+        glLightf(id, GL_SPOT_EXPONENT, 0.0f);
+    }
 
     GLfloat globalAmbientVec[4] = { ambient_intensity , ambient_intensity, ambient_intensity, 1.0f };
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globalAmbientVec);
@@ -342,7 +369,7 @@ int main(int argc, char *argv[])
 
     objects.push_back(load_obj({-8, 0, 5}, "../Low_Poly_Foliage_Pack_001/OBJ Files/Low_Poly_Bush_001.obj"));
     objects.push_back(load_obj({-6, 0, 5}, "../Low_Poly_Foliage_Pack_001/OBJ Files/Low_Poly_Bush_002.obj"));
-    objects.push_back(load_obj({ 0, 0, 10}, "../Low_Poly_Foliage_Pack_001/OBJ Files/Low_Poly_Grass_001.obj"));
+    objects.push_back(load_obj({ 0, 0, 9}, "../Low_Poly_Foliage_Pack_001/OBJ Files/Low_Poly_Grass_001.obj"));
     objects.push_back(load_obj({ 0, 0, 7}, "../Low_Poly_Foliage_Pack_001/OBJ Files/Low_Poly_Grass_002.obj"));
     objects.push_back(load_obj({ 6, 0, 5}, "../Low_Poly_Foliage_Pack_001/OBJ Files/Low_Poly_Tree_Stump_001.obj"));
     objects.push_back(load_obj({ 8, 0, 5}, "../Low_Poly_Foliage_Pack_001/OBJ Files/Low_Poly_Tree_Stump_002.obj"));
